8f39fc55b4ebf6f5a0ce971b0958e64068b39ce8
RAJ KUMAR <rk8210032@gmail.com>
2026-01-14 05:06:14 +0530

    email service anotation added

--- chat-service/src/main/java/com/blink/chatservice/ChatServiceApplication.java
+++ chat-service/src/main/java/com/blink/chatservice/ChatServiceApplication.java
@@ -2,12 +2,16 @@ package com.blink.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class ChatServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServiceApplication.class, args);
+    }
 
 }
